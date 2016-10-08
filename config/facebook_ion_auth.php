bc8bf5d2434b67b4635f1bc053f036d672ee7d9c
Sithu <cithukyaw@gmail.com>
2016-10-08 16:47:02 +0630

    Add default fields option

--- config/facebook_ion_auth.php
+++ config/facebook_ion_auth.php
@@ -7,5 +7,5 @@
 $config['app_id']       = '';       // Your app id
 $config['app_secret']   = '';       // Your app secret key
 $config['scope']        = 'email';  // custom permissions check - http://developers.facebook.com/docs/reference/login/#permissions
-$config['fields']       = '';       // fields to retrieve; if set to '', default is "id,first_name,last_name"
+$config['fields']       = 'id,first_name,last_name,email'; // fields to retrieve; if set to '', default is "id,first_name,last_name"
 $config['redirect_uri'] = '';       // url to redirect back from facebook. If set to '', site_url('') will be used
